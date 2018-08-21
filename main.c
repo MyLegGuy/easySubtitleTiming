55639451b0a86bc7b3fd56ba75815bb2bffbfaa4
MyLegGuy <supermoronslayer@gmail.com>
2018-08-21 04:22:35 -0400

    Prevent buffer overflow

--- main.c
+++ main.c
@@ -166,7 +166,7 @@ FILE* sendMpvCommand(char* msg, char _getOutput){
 void togglePause(){
 	char dresult[256];
 	FILE* fp = sendMpvCommand(GET_PAUSE_STATUS_COMMAND,1);
-	dresult[fread(dresult,sizeof(dresult),1,fp)]='\0';
+	dresult[fread(dresult,sizeof(dresult)-1,1,fp)]='\0';
 	fclose(fp);
 	if (strstr(dresult,"success")!=NULL){
 		if (strstr(dresult,"true")!=NULL){
@@ -241,7 +241,7 @@ double getSeconds(){
 		// Step 1 - Get the info from mpv
 		char dresult[256];
 		FILE* fp = sendMpvCommand(GET_SECONDS_COMMAND,1);
-		dresult[fread(dresult,sizeof(dresult),1,fp)]='\0';
+		dresult[fread(dresult,sizeof(dresult)-1,1,fp)]='\0';
 		fclose(fp);
 		if (strstr(dresult,"success")==NULL){
 			return -1;
